95cd40dcf512d607142f5fae327353624c9928c1
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-02-16 00:47:27 +0100

    Display sunrise and sunset times

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 const cityName = document.getElementById("cityName");
 const currentTemperature = document.getElementById("currentTemperature");
 const weatherDescription = document.getElementById("weatherDescription");
+const sunriseTime = document.getElementById("sunriseTime");
+const sunsetTime = document.getElementById("sunsetTime");
+
+const formateTime = (data) => {
+  const sunriseUnit = data.sys.sunrise;
+  const sunsetUnit = data.sys.sunset;
+  let sunriseConverted = new Date(sunriseUnit * 1000)
+  let sunsetConverted = new Date(sunsetUnit * 1000)
+
+  let sunriseHours = sunriseConverted.getHours();
+  if (sunriseHours < 10) {
+    sunriseHours = `0${sunriseHours}`
+  }
+  let sunriseMinutes = sunriseConverted.getMinutes();
+  if (sunriseMinutes < 10) {
+    sunriseMinutes = `0${sunriseMinutes}`
+  }
+  let sunsetHours = sunsetConverted.getHours();
+  if (sunsetHours < 10) {
+    sunsetHours = `0${sunsetHours}`
+  }
+  let sunsetMinutes = sunsetConverted.getMinutes();
+  if (sunsetMinutes < 10) {
+    sunsetMinutes = `0${sunsetMinutes}`
+  }
+
+  return {
+    sunrise: `${sunriseHours}:${sunriseMinutes}`,
+    sunset: `${sunsetHours}:${sunsetMinutes}`
+  }
+}
 
 const displayCityWeather = (data) => {
   cityName.innerHTML = data.name;
   currentTemperature.innerHTML = `${Math.round(data.main.temp)} ℃`;
-  weatherDescription.innerHTML = data.weather[0].main;
+  weatherDescription.innerHTML = data.weather[0].description;
+  sunriseTime.innerHTML = formateTime(data).sunrise;
+  sunsetTime.innerHTML = formateTime(data).sunset;
 }
 
 const fetchWeatherData = () => {
