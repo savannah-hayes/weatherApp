08a7ed96525f10dac1c1d727cebdfc305817eb85
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-02-16 19:10:15 +0100

    Display seven day forecast

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const currentTemperature = document.getElementById("currentTemperature");
 const weatherDescription = document.getElementById("weatherDescription");
 const sunriseTime = document.getElementById("sunriseTime");
 const sunsetTime = document.getElementById("sunsetTime");
+const dayOfWeek = document.querySelectorAll("#dayOfWeek");
+const forecastWrapper = document.getElementById("forecast");
 
 const formateTime = (data) => {
   const sunriseUnit = data.sys.sunrise;
@@ -41,13 +43,43 @@ const displayCityWeather = (data) => {
   sunsetTime.innerHTML = formateTime(data).sunset;
 }
 
+const displayCityForecast = (data) => {
+  const forecastDays = data.daily;
+  for (let i = 1; i < forecastDays.length; i++) {
+    const daysOfWeek = new Date(forecastDays[i].dt * 1000);
+    const days = daysOfWeek.getDay();
+    const day = [
+      "sun",
+      "mon",
+      "tue",
+      "wed",
+      "thu",
+      "fri",
+      "sat"
+    ]
+
+    const dayNames = day[days];
+    const iconID = forecastDays[i].weather[0].icon;
+    const displayIcon = `https://openweathermap.org/img/wn/${iconID}.png`
+    const maxTemp = Math.round(forecastDays[i].temp.max);
+    const minTemp = Math.round(forecastDays[i].temp.min);
+
+    forecastWrapper.innerHTML += `<div class="forecast-wrapper">
+      <h3>${dayNames}</h3>
+      <img src="${displayIcon}" alt="forecast icon">
+      <p>${maxTemp}/${minTemp}℃</p>
+      </div>`
+  }
+}
+
+
 const fetchWeatherData = () => {
   const city = "stockholm"
   const units = "metric"
   const apiKey = "ad293c37c3b9054770dc9a8a4fe94536"
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
 
-  fetch(apiUrl)
+  fetch(API_URL)
     .then(response => response.json())
     .then(data => {
       displayCityWeather(data)
@@ -55,4 +87,20 @@ const fetchWeatherData = () => {
     .catch(error => console.log(error))
 }
 
+const fetchWeatherForecast = (lat, lon) => {
+  const units = "metric";
+  const exclude = "current,hourly,minutely,alerts"
+  const apiKey = "ad293c37c3b9054770dc9a8a4fe94536";
+  const API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&units=${units}&appid=${apiKey}`;
+
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      displayCityForecast(data)
+    })
+    .catch(error => console.log(error))
+}
+
 fetchWeatherData()
+fetchWeatherForecast("59.32", "18.06")
